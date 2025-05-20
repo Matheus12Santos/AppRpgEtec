446fb36c8f06e86c3366a788f585305cd8084058
MATHEUS VIEIRA <matheus.vieira137@etec.sp.gov.br>
2025-05-19 21:45:30 -0300

    Feito tarefas Arquivo Aula 12 -1

---
+++ AppRpgEtec/Platforms/Android/MainApplication.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Runtime;
+using AppRpgEtec.Models;
+using Javax.Security.Auth.Login;
+using System.Net.Mail;
+
+namespace AppRpgEtec
+{
+    [Application(UsesCleartextTraffic=true)]
+    public class MainApplication : MauiApplication
+    {
+        public MainApplication(IntPtr handle, JniHandleOwnership ownership)
+            : base(handle, ownership)
+        {
+        }
+
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();        
+    }
+}
